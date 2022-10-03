60f991fa1d3221cb130e76831b49996c6198de19
NataliyaSavva <n_savva@list.ru>
2022-10-03 16:05:57 +0300

    Change Sum - Random Summa

--- lesson1/Example003-Sum/Program.cs
+++ lesson1/Example003-Sum/Program.cs
@@ -1,5 +1,5 @@
 ﻿int numberA = new Random().Next(1,10); //1-9
 Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10); //1-9
-Console.Writeline(numberB);
+Console.WriteLine(numberB);
 Console.WriteLine(numberA+numberB);
