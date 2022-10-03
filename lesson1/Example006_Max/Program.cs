36cd5558ed4534e4c773ae7acd05b44cd0c36b5a
NataliyaSavva <n_savva@list.ru>
2022-10-03 16:24:11 +0300

    находим максимальное число

---
+++ lesson1/Example006_Max/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = new Random().Next(1,9);
+int b = new Random().Next(1,9);
+int c = new Random().Next(1,9);
+int d = new Random().Next(1,9);
+int e = new Random().Next(1,9);
+Console.WriteLine(a + " "+ b + " " + c + " " + d + " " + e );
+
+int max = a;
+
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.WriteLine("Максимальное число " + max);
