a8136d734e81e91668293c73366e166587fdd42d
NataliyaSavva <n_savva@list.ru>
2022-10-07 23:32:16 +0300

    добавлен второй урок

---
+++ lesson2/Example03_Method/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] array = {11,12,33,59,84,45,66,17,88,59};
+
+int n =array.Length;
+int find = 59;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+
+
+    index++;
+
+}
